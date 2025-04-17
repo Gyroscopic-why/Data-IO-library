236d3d9cb266c416f02224da86822cd5098311f3
Gyro <gyroscopic.why@gmail.com>
2025-04-17 22:07:56 +0100

    Update GetPath() logic, better Demo
    Improved GetPath() logic
    ADDED:
    - Better custom path parser
    - Safeget  \Documents path
    - Added a fail safe with the safe get LocalAppdata
    - Better error catching and output
    
    Improved the demo
    
    Code refactoring
    Code cleaning & improved readabillity
    
    Added a better gitignore

--- Data saving test/Program.cs
+++ Data saving test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using static System.Console;
 
 using static DataManipulationTest.CustomFunctions;
@@ -9,13 +10,19 @@ namespace DataManipulationTest
     {
         static void Main()
         {
-            string path = GetPath(true, true);
+            //  Get a path for the files
+            string path = GetPath(true, "\\Gyroscopic\\DataManipulation\\TestData", true);
             Write("\n\tChosen path: " + path + "\n\n");
 
+
+            //  Read the data from the demo file
             List<string> data = ReadData(path, "Test data1.db", true);
 
+
+            //  If any data is found
             if (data != null)
             {
+                //  Output the found data
                 Write("\n\tStock read data from file >Test data1.db<:");
                 for (int i = 0; i < data.Count; i++)
                 {
@@ -24,10 +31,15 @@ namespace DataManipulationTest
             }
             WaitForKey();
 
+            
+            //  Parse the read data
             List<string> parsedData = ParseData(data, true, true, "$@", "#", "$@", true);
 
+
+            //  If the parsing was successful
             if (parsedData != null)
             {
+                //  Ouput the parsed data
                 Write("\n\tParsed data from file >Test data1.db<:");
                 for (int i = 0; i < parsedData.Count; i++)
                 {
@@ -37,16 +49,19 @@ namespace DataManipulationTest
             WaitForKey();
 
 
-
+            //  Save the parsed data to a new file
             SaveData(path, "Test data2.db", data, true, true);
             WaitForKey();
 
 
-
+            //  Read the data from the temporary file
             data = ReadData(path, "Test data2.db", true);
 
+
+            //  If the data read was successful
             if (data != null)
             {
+                //  Output the read data
                 Write("\n\tStock read data from file >Test data2.db<:");
                 for (int i = 0; i < data.Count; i++)
                 {
@@ -55,8 +70,12 @@ namespace DataManipulationTest
             }
             WaitForKey();
 
+
+            //  Parse the read data
             parsedData = ParseData(data, true, true, "$@", "#", "$@", true);
 
+
+            //  If the parsing was successful
             if (parsedData != null)
             {
                 Write("\n\tParsed data from file >Test data2.db<:");
@@ -68,13 +87,30 @@ namespace DataManipulationTest
             WaitForKey();
 
 
-
-            ClearData(path, "Test data2.db", true);
+            //  Clear the temporary file
+            ClearFile(path, "Test data2.db", true);
             WaitForKey();
 
 
+            //  Read the data from the new file
+            data = ReadData(path, "Test data2.db", true);
 
-            DeleteData(path, "Test data2.db", true);
+
+            //  If the data read was successful
+            if (data != null)
+            {
+                //  Output the read data
+                Write("\n\tStock read data from file >Test data2.db<:");
+                for (int i = 0; i < data.Count; i++)
+                {
+                    Write("\n\t\tdata[" + i + "] = " + data[i]);
+                }
+            }
+            WaitForKey();
+
+
+            //  Delete the temporary file
+            DeleteFile(path, "Test data2.db", true);
             Write("\n\tTest finished. Press any key to exit ");
             ReadKey();
         }
